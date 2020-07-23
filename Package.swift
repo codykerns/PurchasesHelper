edfb917d1595ebd8cf7a78ad9b35d863b0818a0f
Cody Kerns <cody@revenuecat.com>
2020-07-23 14:53:39 -0400

    Set minimum iOS and watchOS versions

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PurchasesHelper",
     platforms: [
-        .macOS(.v10_12), .iOS(.v9), .watchOS("6.2"), .tvOS(.v9)
+        .iOS("11.2"), .watchOS("6.2")
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
