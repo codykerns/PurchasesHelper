326e25880b20008608869892bcedcb6ec6fe9034
Cody Kerns <cody@revenuecat.com>
2020-07-23 14:45:21 -0400

    Updated minimum supported Purchases version to 3.1.0

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PurchasesHelper",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v9), .watchOS("6.2"), .tvOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "3.0.0"),
+        .package(url: "https://github.com/RevenueCat/purchases-ios.git", from: "3.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
