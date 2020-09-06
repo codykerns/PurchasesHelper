f48a7d05b20576e787419abb15ed6756d22c486a
Cody Kerns <44073103+codykerns@users.noreply.github.com>
2020-09-06 15:14:24 -0400

    Update Package.swift
    Add support for macOS v11 (Big Sur)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PurchasesHelper",
     platforms: [
-        .iOS("9.3"), .watchOS("6.2")
+        .iOS("9.3"), .watchOS("6.2"), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
