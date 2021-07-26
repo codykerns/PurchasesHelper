4541a6804b41c5eeb1efd6dff95274907e2710b2
Cody Kerns <cody@kerns.us>
2021-07-26 17:20:58 -0400

    Clarifies package terms with better documentation. Adds support for excluding intro prices in package terms.

--- Sources/PurchasesHelper/PackageExtensions.swift
+++ Sources/PurchasesHelper/PackageExtensions.swift
@@ -7,7 +7,13 @@
 import Purchases
 
 public extension Purchases.Package {
-    func packageTerms(recurring: Bool = true) -> String {
+    
+    /// Customer-facing payment terms for a RevenueCat package. Not localized.
+    /// - Parameter isRecurring: If the terms should be in a recurring format. `$99/year` vs `$99 for 1 year`. Defaults to `true`.
+    /// - Parameter includesIntroTerms: If the terms should include the introductory price for the product. e.g. if a user has already redeemed a free trial, passing `false` would not include the free trial terms. Defaults to `true`.
+    /// - Returns: A string with the terms of the package's product.
+    /// e.g. `1 week free trial, then $24.99/year`
+    func packageTerms(isRecurring: Bool = true, includesIntroTerms: Bool = true) -> String {
         let normalPrice = self.localizedPriceString
         
         guard self.packageType != .lifetime else {
@@ -19,12 +25,12 @@ public extension Purchases.Package {
         // - setup the normal product subscription length string
         var perTitle = "/\(packagePerTitle)".lowercased()
         
-        if recurring == false {
+        if isRecurring == false {
             perTitle = "for \(displayTitle)".lowercased()
         }
         
         // - check for an introductory discount for a package
-        if #available(iOS 11.2, *),
+        if #available(iOS 11.2, *), includesIntroTerms == true,
             let intro = self.product.introductoryPrice {
             
             // - introductory offers
@@ -45,7 +51,7 @@ public extension Purchases.Package {
                 // - pay as you go title
                 // - e.g. '$1.99 per month for 6 months, then $24.99/year'
                 let payAYGTitle = intro.periodLengthTitle(for: intro.subscriptionPeriod.unit)
-                let introPerTitle = "\(introPrice)/\(intro.subscriptionPeriod.unit.title) for \(payAYGTitle)"
+                let introPerTitle = "\(introPrice)/\(intro.subscriptionPeriod.unit.unitTitle) for \(payAYGTitle)"
                 return "\(introPerTitle), then \(normalPrice)\(perTitle)"
                 
             @unknown default:
@@ -93,7 +99,7 @@ public extension Array where Element: Purchases.Package {
 fileprivate extension SKProductSubscriptionPeriod {
     var periodLengthTitle: String {
         let isPlural = numberOfUnits != 1
-        return "\(numberOfUnits) \(unit.title)\(isPlural ? "s" : "")"
+        return "\(numberOfUnits) \(unit.unitTitle)\(isPlural ? "s" : "")"
     }
 }
 
@@ -101,13 +107,13 @@ fileprivate extension SKProductSubscriptionPeriod {
 fileprivate extension SKProductDiscount {
     func periodLengthTitle(for unit: SKProduct.PeriodUnit) -> String {
         let isPlural = numberOfPeriods != 1
-        return "\(numberOfPeriods) \(unit.title)\(isPlural ? "s" : "")"
+        return "\(numberOfPeriods) \(unit.unitTitle)\(isPlural ? "s" : "")"
     }
 }
 
 @available(iOS 11.2, *)
 fileprivate extension SKProduct.PeriodUnit {
-    var title: String {
+    var unitTitle: String {
         switch self {
         case .day:
             return "day"
