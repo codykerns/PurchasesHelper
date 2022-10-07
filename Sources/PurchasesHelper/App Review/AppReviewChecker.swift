6ede14fbee23d1eb190c46aac376111b9fc7ca8d
Cody Kerns <cody@kerns.us>
2022-10-06 22:29:58 -0400

    don't eagerly fetch from the api, send to main thread

--- Sources/PurchasesHelper/App Review/AppReviewChecker.swift
+++ Sources/PurchasesHelper/App Review/AppReviewChecker.swift
@@ -28,11 +28,7 @@ public class AppReviewChecker {
     }
 
     /// The Apple ID of your app. Found in App Store Connect -> App -> App Information
-    private var appleAppId: String? {
-		didSet {
-			getLiveAppStoreVersion()
-		}
-	}
+    private var appleAppId: String?
 
     /// Use this value to force AppReviewChecker to pretend your app is in review. When shipping your app, set this to `shippingApp`
     public var testingStatus: TestingStatus = .shippingApp
@@ -83,42 +79,43 @@ public class AppReviewChecker {
             self.log("Fetching current app version..")
 
 			let task = URLSession.shared.dataTask(with: currentVersionLookupURL) { (data, response, error) in
-				
-				if let data = data {
-					do {
-						// Convert the data to JSON
-						let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
-						
-						// Get Live App Store Version
-						if let json = jsonSerialized, let results = json["results"] as? [[String: Any]] {
+                DispatchQueue.main.async {
+                    if let data = data {
+                        do {
+                            // Convert the data to JSON
+                            let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
 
-                            if results.count > 0,
-                               let version = results[0]["version"] as? String {
-                                self.log("Found current live version of: \(version)")
-                                self.liveVersion = version
+                            // Get Live App Store Version
+                            if let json = jsonSerialized, let results = json["results"] as? [[String: Any]] {
+
+                                if results.count > 0,
+                                   let version = results[0]["version"] as? String {
+                                    self.log("Found current live version of: \(version)")
+                                    self.liveVersion = version
+                                } else {
+                                    self.log("Unable to find app based on the Apple ID: \(appleAppId)")
+                                    self.setToDefaultUnknown()
+                                }
+
+                                completion?()
+
                             } else {
-                                self.log("Unable to find app based on the Apple ID: \(appleAppId)")
+                                self.log("Invalid data from Apple's API.")
                                 self.setToDefaultUnknown()
+                                completion?()
+
                             }
-
-                            completion?()
-
-						} else {
-                            self.log("Invalid data from Apple's API.")
+                        }  catch let error as NSError {
+                            self.log("Failed to decode API response: \(error.localizedDescription).")
                             self.setToDefaultUnknown()
                             completion?()
-
-						}
-					}  catch let error as NSError {
-                        self.log("Failed to decode API response: \(error.localizedDescription).")
+                        }
+                    } else if let error = error {
+                        self.log("Failed to fetch data: \(error.localizedDescription).")
                         self.setToDefaultUnknown()
                         completion?()
-					}
-				} else if let error = error {
-                    self.log("Failed to fetch data: \(error.localizedDescription).")
-                    self.setToDefaultUnknown()
-                    completion?()
-				}
+                    }
+                }
 			}
 			
 			task.resume()
